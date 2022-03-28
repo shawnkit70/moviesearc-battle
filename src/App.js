956ff1836c0f28ac56e72f12d07a394af79f9a2a
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-03-28 20:25:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 
+const URL = "http://www.omdbapi.com/?i=tt3896198&apikey=d4c62730" ;
 
 function App() {
   return (
     <div className="App">
-     
+     {URL}
     </div>
   );
 }
