c1bc8d1c24c8dc8bb2da369439b898851881c53d
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-04-02 16:55:00 +0530

    Main changes done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css' ;
 import './App.css' ;
 import SearchMovie from "./components/SearchMovie";
 
-const URL = "http://www.omdbapi.com/?s=pokemon&apikey=d4c62730" ;
 
 export default function App() {
   
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row'>
-				<SearchMovie url = {URL} />
+				<SearchMovie movie="First" />
+			</div>
+			<div>
+				<SearchMovie movie="Second"/>
 			</div>
 		</div>
 	);
--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,52 +1,73 @@
 import React from "react" ; 
 import "../App.css" ;
-import Dropdown from 'react-bootstrap/Dropdown'
+import Dropdown from 'react-bootstrap/Dropdown' ;
 
 export default function SearchMovie ( props ) {
+    const [ hide , setHide ] = React.useState(false) ;
     const [ search, setSearch ] = React.useState("") ;
-    const [ movies, setMovies ] = React.useState([{
-        "Title": "Star Wars: Episode IV - A New Hope",
-        "Year": "1977",
-        "imdbID": "tt0076759",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    },
-    {
-        Title: 'Star Wars: Episode VI - Return of the Jedi',
-        Year: '1983',
-        imdbID: 'tt0086190',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-    }]);
+    const [ movies, setMovies ] = React.useState([]);
 
-    const getMovieInfo = async( query )=> {
+    const [movie , setMovie ] = React.useState([]) ;
+
+    const getMoviesInfo = async( query )=> {
         
         const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=d4c62730`) ;
         const responseJson = await response.json() ;
         console.log(responseJson) ;
         if( responseJson.Response === "True" ){
-            setMovies(responseJson.Search) ;
+            const filteredMovie = Array.from(new Set(responseJson.Search.map(a => a.imdbID)))
+            .map(id => {
+            return responseJson.Search.find(a => a.imdbID === id)
+            })
+            console.log(filteredMovie) ;
+            setMovies(filteredMovie) ;
+            setHide( prevValue => !prevValue ) ;
         }
         else{
             setMovies( movies ) ;
         }
         }
-     React.useEffect(()=>{
-         const data = setTimeout( ()=> getMovieInfo( search ) , 500) ;
+
+    const getMovieInfo = async( id ) =>{
+            const response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=d4c62730`)
+            const selectedMovie = await response.json() ;
+            console.log( selectedMovie ); 
+            const {Title,Year,Rated,Awards,BoxOffice,imdbRating,imdbVotes} = selectedMovie ;
+            setMovie( prevValue => ({...prevValue , Title , Year , Rated , Awards , BoxOffice , imdbRating , imdbVotes } ) ) ;
+            console.log(movie) ;
+        }
+    
+    const handleValue = (e) => {
+      const movId = e.target.children[1].defaultValue ;
+      getMovieInfo( movId ) ;
+      setMovies([]) ;
+      setHide( prevValue => !prevValue ) ;
+    }
+    
+    
+    React.useEffect(()=>{
+         const data = setTimeout( ()=> getMoviesInfo( search ) , 500) ;
          return () => clearTimeout(data);
-     } , [search])
+     } , [ search ])
+
+     
     
     return (
         <div>
-            <input type="text" placeholder="Search your First movie" 
+            <input type="text" placeholder={`Search your ${props.movie} movie`}
             value={search} onChange={ event => setSearch(event.target.value)}/>
-            <div className="movieList">
-            <Dropdown.Menu show> {movies
-            .map( item => <Dropdown.Item eventKey={item.imdbID} onClick={ event => console.log(event) }>
-            <img alt={ item.Title } src={item.Poster} />
+            <div className="movieList" style={{display: hide ? "block" :"none"}}>
+            <Dropdown.Menu show > {movies
+            .map( item => <Dropdown.Item onClick={handleValue} >
+            <img alt={ item.Title } src={ item.Poster } />
+            <input hidden defaultValue={item.imdbID} />
             <h3>{item.Title}</h3></Dropdown.Item>)}</Dropdown.Menu>
             </div>
+            <div>
+        {Object.entries(movie).map(([key, value]) =>
+            <p>{key} : {value}</p>
+        )}
+    </div>
         </div>
     )
 }
